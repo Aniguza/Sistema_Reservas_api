b4fd2e94ab6c20573e3d5d92119b513e671251b8
Aniguza <guzmanana0106@gmail.com>
2025-12-20 02:23:45 -0500

    fix: update CORS configuration for development and production environments

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // Configurar CORS DEV
-  app.enableCors({ 
-    origin: ['http://localhost:5174', 'http://localhost:5173'] 
-  });
+  // app.enableCors({ 
+  //   origin: ['http://localhost:5174', 'http://localhost:5173'] 
+  // });
 
   // Configurar cors PROD
-  // app.enableCors({ 
-  //   origin: ['https://utpreservas.netlify.app', 'https://utpreservasadmin.netlify.app'] 
-  // });
+  app.enableCors({ 
+    origin: ['https://utpreservas.netlify.app', 'https://utpreservasadmin.netlify.app'] 
+  });
   await app.listen(process.env.PORT ?? 3000);
 
   // console.log('🚀 Servidor iniciado en http://localhost:3000');
