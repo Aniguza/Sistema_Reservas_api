c8144d8440c3cfbb4e2ebf8aeae221b10de3a777
Aniguza <guzmanana0106@gmail.com>
2025-12-30 01:41:28 -0500

    fix: update route for fetching user by ID to use a simpler path

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -64,7 +64,7 @@ export class UsuariosController {
     }
 
     // Obtener usuario por ID (TODOS los roles autenticados)
-    @Get('/id/:id')
+    @Get('/:id')
     @UseGuards(JwtAuthGuard)
     async getUsuarioById(
         @Res() response,
