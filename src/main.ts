f866c59eb12f612a492fb4545769cd2881dad98d
Aniguza <guzmanana0106@gmail.com>
2026-01-17 00:30:19 -0500

    feat: Actualizar configuración de CORS y mejorar mensajes de inicio del servidor
    - Se configuró CORS para permitir el acceso desde dominios de producción.
    - Se restauraron y mejoraron los mensajes de inicio del servidor para listar los endpoints disponibles.

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,24 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // Configurar CORS DEV
-  app.enableCors({ 
-    origin: ['http://localhost:5174', 'http://localhost:5173'] 
-  });
+  // app.enableCors({ 
+  //   origin: ['http://localhost:5174', 'http://localhost:5173'] 
+  // });
 
   // Configurar cors PROD
-  // app.enableCors({ 
-  //   origin: ['https://localhos', 'https://intranet.reservaslabutp.site'] 
-  // });
+  app.enableCors({ 
+    origin: ['https://reservaslabutp.site', 'https://intranet.reservaslabutp.site'] 
+  });
   await app.listen(process.env.PORT ?? 3000);
 
-  // console.log('🚀 Servidor iniciado en http://localhost:3000');
-  // console.log('📋 Endpoints disponibles:');
-  // console.log('  - POST /usuarios - Crear usuario');
-  // console.log('  - GET /usuarios - Listar usuarios');
-  // console.log('  - POST /auth/login - Login usuarios');
-  // console.log('  - POST /auth/admin/login - Login administrador');
-  // console.log('  - GET /auth/profile - Perfil (requiere token)');
-  // console.log('  - POST /analytics/tiempo-formulario - Registrar métricas de tiempo');
-  // console.log('  - GET /analytics/tiempo-formulario/stats - Obtener estadísticas');
+  console.log('🚀 Servidor iniciado en http://localhost:3000');
+  console.log('📋 Endpoints disponibles:');
+  console.log('  - POST /usuarios - Crear usuario');
+  console.log('  - GET /usuarios - Listar usuarios');
+  console.log('  - POST /auth/login - Login usuarios');
+  console.log('  - POST /auth/admin/login - Login administrador');
+  console.log('  - GET /auth/profile - Perfil (requiere token)');
+  console.log('  - POST /analytics/tiempo-formulario - Registrar métricas de tiempo');
+  console.log('  - GET /analytics/tiempo-formulario/stats - Obtener estadísticas');
 }
 bootstrap();
